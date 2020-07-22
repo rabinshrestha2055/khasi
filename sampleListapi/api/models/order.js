ec433ae7ac91ee73499a61c78877e9ec29866edd
Aashish <ashishdahal490@gmail.com>
2020-07-22 18:14:35 +0545

    khasiapi

--- sampleListapi/api/models/order.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-const orderSchema=mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
-     product:{type:mongoose.Schema.Types.ObjectId,ref:'product'},
-     quantity:{type:Number,default:1}
-});
-module.exports=mongoose.model('product',orderSchema);
